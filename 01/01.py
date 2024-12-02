e83986422d066c6e8539b07151fa7ee283b7301f
Van Nguyen <van.nguyenn@protonmail.com>
2024-12-02 17:58:39 +0100

    day 01 and 02 second parts

--- 01/01.py
+++ 01/01.py
@@ -1,18 +1,39 @@
-input_file = "input.txt"
+from collections import Counter
 
+input_file = "input.txt"
 left_list = []
 right_list = []
+# first part
 
+# with open(input_file, "r") as file:
+#     for line in file:
+#         left, right = map(int, line.split())
+#         left_list.append(left)
+#         right_list.append(right)
+
+
+# left_list.sort()
+# right_list.sort()
+
+# total_distance = sum(abs(left - right) for left, right in zip(left_list, right_list))
+
+# print(total_distance)
+
+# second part
+similarity = 0
+total_similarity = 0
 with open(input_file, "r") as file:
     for line in file:
         left, right = map(int, line.split())
         left_list.append(left)
         right_list.append(right)
 
+        occurence = Counter(right_list)
 
-left_list.sort()
-right_list.sort()
+    for value in left_list:
+        if value in right_list:
+            similarity = value * occurence[value]
+            # print(similarity)
+            total_similarity = total_similarity + similarity
 
-total_distance = sum(abs(left - right) for left, right in zip(left_list, right_list))
-
-print(total_distance)
+    print(total_similarity)
--- 02/02.py
+++ 02/02.py
@@ -1,3 +1,4 @@
+# first part
 def check_level(level):
     is_increasing = all(level[i] < level[i + 1] for i in range(len(level) - 1))
     is_decreasing = all(level[i] > level[i + 1] for i in range(len(level) - 1))
@@ -9,10 +10,35 @@ def check_level(level):
     return (is_increasing or is_decreasing) and valid_range
 
 
+def check_level_two(level):
+    count = 0
+    for i in range(len(level)):
+        new_level = level[:i] + level[i + 1 :]
+
+        is_increasing = all(
+            new_level[j] < new_level[j + 1] for j in range(len(new_level) - 1)
+        )
+
+        is_decreasing = all(
+            new_level[j] > new_level[j + 1] for j in range(len(new_level) - 1)
+        )
+
+        valid_range = all(
+            1 <= abs(new_level[j] - new_level[j + 1]) <= 3
+            for j in range(len(new_level) - 1)
+        )
+
+        if (is_increasing or is_decreasing) and valid_range:
+            count += 1
+
+    return count
+
+
 level_file = "input.txt"
 
 with open(level_file, "r") as file:
     levels = [list(map(int, line.split())) for line in file]
-    safe_reports = sum(1 for level in levels if check_level(level))
+    safe_reports = sum(1 for level in levels if check_level_two(level))
+
 
 print(safe_reports)
